250bf69596dda57520edfb815f037b3cecbc73a3
Kevin <kevinj@theodo.fr>
2017-08-20 20:12:34 +0200

    Offline assets

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,32 @@
 const CACHE_NAME = 'Moovz';
+const urls = [
+  '/',
+  '/manifest.json',
+  '/favicon.ico',
+  '/static/js/bundle.js',
+  '/favicon.ico',
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('cache open');
+      .then(cache => {
+        return cache.addAll(urls)
       })
   );
 });
+
+self.addEventListener('fetch', event => {
+  cacheFallingBackToNetwork(event)
+});
+
+
+const cacheFallingBackToNetwork = event => event.respondWith(
+  caches.match(event.request)
+    .then(response => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+);
