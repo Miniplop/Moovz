cb3f8e1e13f4a3e592186b604d5af7abba97e38f
Kevin <kevinj@theodo.fr>
2017-08-22 22:07:54 +0200

    Start part 3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
 import React, { Component } from 'react';
+import request from 'superagent';
+import { values } from 'lodash';
 import './App.css';
 
+const styles = {
+  fullWidth: {
+    width: '100%',
+    maxWidth: 500,
+  }
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {listItem: []};
+  }
+
+  componentDidMount() {
+    this.fetch();
+  }
+
+  fetch() {
+    return request.get('https://cropchat-50ff7.firebaseio.com/cat.json')
+      .then(data => {
+        this.setState({ listItem: values(data.body) });
+      });
+  }
+
+  renderList = () => (
+    this.state.listItem.map((item, index) => (
+        <div style={styles.fullWidth} key={index}>
+          <img style={styles.fullWidth} src={item.url} alt="hello"/>
+          <div>{item.comment}</div>
+        </div>
+      )
+    )
+  );
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          {this.state.listItem.length > 0 && this.renderList()}
+        </div>
       </div>
     );
   }
